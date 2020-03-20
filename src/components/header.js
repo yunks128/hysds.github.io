b4e74a9640e076c729d2fbde203b7e64edba9f73
safiyahlakhany <salakhany@gmail.com>
2020-03-19 17:03:35 -0700

    Navbar UI

--- src/components/header.js
+++ src/components/header.js
@@ -3,25 +3,37 @@ import { Link } from 'gatsby'
 import logo from '../images/hysds-logo.png'
 
 export default () => (
-
-  
-    <nav class="navbar">
-
-      <div id="logo">
-        <Link to="/">
-          <img src={logo} className="header-logo" alt="..." />
-        </Link>
-      </div>
-      <ul class="nav justify-content-end">
-        <li class="nav-item"> <Link to="/about/" id="nav-item">ABOUT</Link> </li>
-        <li class="nav-item"> <Link to="/features/" id="nav-item">FEATURES</Link> </li>
-        <li class="nav-item"> <Link to="/contribute/" id="nav-item">CONTRIBUTE</Link> </li>
-        <li class="nav-item"> <Link to="/docs/hysds/hysds-overview/" id="nav-item">DOCS</Link> </li>
-      </ul>
-    </nav>
-
-
-  
-
- 
+  <nav class="navbar">
+    <div id="logo">
+      <Link to="/">
+        <img src={logo} className="header-logo" alt="..." />
+      </Link>
+    </div>
+    <ul class="nav justify-content-end">
+      <li class="nav-item">
+        {' '}
+        <Link to="/about/" id="nav-item">
+          ABOUT
+        </Link>{' '}
+      </li>
+      <li class="nav-item">
+        {' '}
+        <Link to="/features/" id="nav-item">
+          FEATURES
+        </Link>{' '}
+      </li>
+      <li class="nav-item">
+        {' '}
+        <Link to="/contribute/" id="nav-item">
+          CONTRIBUTE
+        </Link>{' '}
+      </li>
+      <li class="nav-item">
+        {' '}
+        <Link to="/docs/hysds/hysds-overview/" id="nav-item">
+          DOCS
+        </Link>{' '}
+      </li>
+    </ul>
+  </nav>
 )

db425d625eb7c6ffc0a94125f76c11e2d10abaf5
Sehoon Park <sehoon.s.park@gmail.com>
2020-03-23 10:22:50 -0700

    Refactor footer ui component to use .yaml file

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
+
 import logo from '../images/hysds-logo.png'
+import footerData from '../../content/footer.yaml'
 
 export default () => (
   <footer className="pt-4 my-md-5 pt-md-5 border-top">
@@ -12,91 +14,20 @@ export default () => (
           {new Date().getFullYear()}
         </small>
       </div>
-      <div className="col-6 col-md">
-        <h5>Features</h5>
-        <ul className="list-unstyled text-small">
-          <li>
-            <a className="text-muted" href="#">
-              Cool stuff
-            </a>
-          </li>
-          <li>
-            <a className="text-muted" href="#">
-              Random feature
-            </a>
-          </li>
-          <li>
-            <a className="text-muted" href="#">
-              Team feature
-            </a>
-          </li>
-          <li>
-            <a className="text-muted" href="#">
-              Stuff for developers
-            </a>
-          </li>
-          <li>
-            <a className="text-muted" href="#">
-              Another one
-            </a>
-          </li>
-          <li>
-            <a className="text-muted" href="#">
-              Last time
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="col-6 col-md">
-        <h5>Resources</h5>
-        <ul className="list-unstyled text-small">
-          <li>
-            <a className="text-muted" href="#">
-              Resource
-            </a>
-          </li>
-          <li>
-            <a className="text-muted" href="#">
-              Resource name
-            </a>
-          </li>
-          <li>
-            <a className="text-muted" href="#">
-              Another resource
-            </a>
-          </li>
-          <li>
-            <a className="text-muted" href="#">
-              Final resource
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div className="col-6 col-md">
-        <h5>About</h5>
-        <ul className="list-unstyled text-small">
-          <li>
-            <a className="text-muted" href="#">
-              Team
-            </a>
-          </li>
-          <li>
-            <a className="text-muted" href="#">
-              Locations
-            </a>
-          </li>
-          <li>
-            <a className="text-muted" href="#">
-              Privacy
-            </a>
-          </li>
-          <li>
-            <a className="text-muted" href="#">
-              Terms
-            </a>
-          </li>
-        </ul>
-      </div>
+      {footerData.map(section => (
+        <div className="col-6 col-md">
+          <h5>{section.title}</h5>
+          <ul className="list-unstyled text-small">
+            {section.items.map(item => (
+              <li>
+                <Link className="text-muted" to={item.to}>
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   </footer>
 )

b2585ad1124430acd356ce7847dd92644b0a6676
safiyahlakhany <salakhany@gmail.com>
2020-04-07 07:25:52 -0700

    Added files

---
+++ src/components/banner.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import logo from '../images/satellite.jpg'
+
+export default () => (
+  <div class="satellite-banner">
+    <img src={logo} class="satellite-img" />
+  </div>
+)
